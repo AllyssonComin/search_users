cc8033f0b92bc9093459b83e726a57e7363d0c6d
Allysson Comin <allysson_luan@hotmail.com>
2022-03-25 16:32:55 -0400

    create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "json"
+require "open-uri"
+
+url = "https://randomuser.me/api/?results=30&seed=giga&format=json&inc=gender,name,email,Picture&nat=br"
+user_serialized = URI.open(url).read
+user_api = JSON.parse(user_serialized)
+
+results = user_api["results"]
+
+results.each do |user|
+  User.create(
+    name: "#{user['name']['title']} #{user['name']['first']} #{user['name']['last']}",
+    gender: "#{user['gender']}",
+    email: "#{user['email']}"
+  )
+end
